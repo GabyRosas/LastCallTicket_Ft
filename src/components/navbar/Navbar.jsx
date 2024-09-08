fb50f7fa97cd78be180f97c6f593ca44a2b4966f
GabyRosas <fleuryrosas@gmail.com>
2024-09-08 19:17:12 +0200

    first commit with react & folders structure

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <navbar className="navbar">
+            <h1 id="LastCTh1">LastCallTicket</h1>
+            <img src="/public/images/logolct.png" alt="Logo del sitio"  />
+            </navbar>
+    );
+}
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <>
+            <h1>Pagina home de ejemplo</h1>
+        </>
+    )
+}
+
+export default Home;
